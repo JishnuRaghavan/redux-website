d498f626df20670db8d7b1c3c653602b7d1b92e1
JishnuRaghavan <jishu18js@gmail.com>
2024-02-07 17:45:20 +0530

    profile delete and sign out

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,27 +1,34 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-  username:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  email:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  password:{
-    type:String,
-    required:true
-  },
-  profilePicture:{
-    type:String,
-    default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        
+    },
+    profilePicture:{
+        type:String,
+        default: 'https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg',
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+        required:true,
+    },
+    
+  
+},{timestamps:true});
 
-  }
-},{timestamps:true})
-
-const User = mongoose.model('User',userSchema)
+const User = mongoose.model('User', userSchema);
 
 export default User;
